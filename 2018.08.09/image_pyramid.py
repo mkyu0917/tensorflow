7f14339a056230d71556b2dfddd86fcba4d6eceb
mkyu0917 <mkyu0917@gmail.com>
2018-08-09 14:33:46 +0900

    image_control, merge

---
+++ 2018.08.09/image_pyramid.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+def pyramid():
+    img = cv2.imread('D:\PycharmProject\data\suji.jpg',cv2.IMREAD_ANYCOLOR)
+    tmp = img.copy()
+
+    win_titles =['org', 'level1', 'level2', 'level3']
+    g_down = []
+    g_down.append(tmp)
+    for i in range(3):
+        tmp1 = cv2.pyrDown(tmp)
+        g_down.append(tmp1)
+        tmp = tmp1
+
+    for i in range(4):
+        cv2.imshow(win_titles[i], g_down[i])
+    cv2.waitKey(0)
+    cv2.destoryAllWindows()
+pyramid()
